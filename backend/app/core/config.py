4b5d94db068cbbbd75f01ed4dd1d6b330637e0f2
ColtenOuO <jun930436@gmail.com>
2025-12-15 01:02:55 +0800

    env: init setting

---
+++ backend/app/core/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Parallel Online Judge API"
+    API_V1_STR: str = "/api/v1"
+    
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "password"
+    POSTGRES_SERVER: str = "localhost"
+    POSTGRES_DB: str = "poj_db"
+    
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
